161fa21685284aaf16a9e301e90d01c74940ce26
codingjin <475166289@qq.com>
2022-07-22 10:32:30 -0700

    d

--- qemu_debootstrap.sh
+++ qemu_debootstrap.sh
@@ -12,5 +12,29 @@ sudo debootstrap --arch=amd64 --include="sudo vim bash-completion net-tools etht
 
 sudo umount mountdir
 
+#sudo mount -t proc /proc mountdir/proc
+#sudo mount -t sysfs /sys mountdir/sys
+#sudo mount -o bind /dev mountdir/dev
+#sudo mount -o bind /dev/pts mountdir/dev/pts
+
+
+#################chroot
+sudo mount -o loop img mountdir
+sudo chroot mountdir
+#passwd
+
+#sudo update
+
+#ifconfig -a
+
+#ping -c2 google.com
+#exit
+#################out
+
+#sudo umount mountdir/proc
+#sudo umount mountdir/sys
+#sudo umount mountdir/dev/pts
+#sudo umount mountdir/dev
+
 
 
