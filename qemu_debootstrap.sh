b79fdbded863fe1c04a58137f93580ed39967f56
codingjin <475166289@qq.com>
2022-07-22 09:28:56 -0700

    dv

--- qemu_debootstrap.sh
+++ qemu_debootstrap.sh
@@ -8,7 +8,7 @@ mkdir mountdir
 
 sudo mount -o loop img mountdir
 
-sudo debootstrap --arch=amd64 --include="sudo vim bash-completion net-tools ethtool ifupdown network-manager iputils-ping rsyslog udev systemd" bionic mountdir "http://archive.ubuntu.com/ubuntu https://docstore.mik.ua/manuals/ubuntu/8.04/installation-guide/hppa/linux-upgrade.html"
+sudo debootstrap --arch=amd64 --include="sudo vim bash-completion net-tools ethtool ifupdown network-manager iputils-ping rsyslog udev systemd" bionic mountdir "http://archive.ubuntu.com/ubuntu"
 
 sudo umount mountdir
 
