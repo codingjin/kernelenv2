041a31a61229509de387548aaa7c2d5fc4a40151
codingjin <55301130+codingjin@users.noreply.github.com>
2022-07-24 09:39:58 -0700

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 make menuconfig
-#make x86_64_defconfig
-#make kvm_guest.config
+
+# in order to support vm Internet access, at least now, I need to config the E1000
+# Location: Device Drivers -> Network device support ->Ethernet driver support -> Intel devices
+# Set all the options under Intel devices into “y/*”, 'M' does not work!
+
+
 
 make -j$(nproc)
 cp ./arch/x86_64/boot/bzImage .
